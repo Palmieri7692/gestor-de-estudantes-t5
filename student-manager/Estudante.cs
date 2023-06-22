adcb77b3f0bdf9c23a8bd9367e4f67e5c42d19a0
Palmieri7692 <118479734+Palmieri7692@users.noreply.github.com>
2023-06-22 18:10:37 -0300

    TI
    fizemos as configurações que faltavam no cadastro

--- student-manager/Estudante.cs
+++ student-manager/Estudante.cs
@@ -17,12 +17,27 @@ namespace student_manager
             MySqlCommand comando = new MySqlCommand("INSERT INTO `estudantes`(`nome`, `sobrenome`, `nascimento`, `genero`, `telefone`, `endereco`, `foto`) VALUES (@nm,@snm,@nsc,@gen,@tel,@end,@ft)", meuBancoDeDados.getConexao)
            comando.Parameters.Add("@nm", MySqlDbType.VarChar).Value = nome;
             comando.Parameters.Add("@snm", MySqlDbType.VarChar).Value = sobrenome;
+            comando.Parameters.Add("@nsc", MySqlDbType.Date).Value = nascimento;
             comando.Parameters.Add("@gen", MySqlDbType.VarChar).Value = genero;
             comando.Parameters.Add("@tel", MySqlDbType.VarChar).Value = telefone;
             comando.Parameters.Add("@end", MySqlDbType.Text).Value = endereco;
-            comando.Parameters.Add("@ft", MySqlDbType.LongBlob).Value = foto;
+            comando.Parameters.Add("@ft", MySqlDbType.LongBlob).Value = foto.ToArray();
+
+            meuBancoDeDados.abrirConexao();
+            if (comando.EndExecuteNonQuery() == 1)
+
+            {
+             meuBancoDeDados.fecharConexao();
+                return true;
+            }
+            else
+            {
+                meuBancoDeDados.fecharConexao();
+                return false;
+            }
+            
            
-            return false;
+           
 
         }
     }
